7cee0c1c01baa9f5d7569b4d6f2c11ae41b2205c
VaishnaviVarma04 <vaishnavi2004varma@gmail.com>
2026-05-07 05:36:55 +0530

    Final Commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:5000/api' });
+const api = axios.create({ baseURL: 'https://project-hub-production-f5b4.up.railway.app/api' });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
